2c5930f9f8fb6dc4ca8e79e32f297acd3fb0bfa7
Vadim Korabelnik <vad@oblako9.ru>
2019-05-03 21:36:20 +0300

    Must resolve .jsx import issue within test files

--- src/components/app/app.test.js
+++ src/components/app/app.test.js
@@ -2,7 +2,6 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import {App} from './app.jsx';
 
-
 it(`App correctly renders after relaunch`, () => {
   const tree = renderer
     .create(<App
