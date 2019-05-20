f396fca437b3890961867715a2cefe4b41905c8c
Vadim Korabelnik <vad@oblako9.ru>
2019-05-20 15:12:18 +0300

    Added AudioPlayers with reset state on render

---
+++ src/components/audio-player/audio-player.jsx
@@ -0,0 +1,70 @@
+import React, {PureComponent, Fragment} from 'react';
+import PropTypes from 'prop-types';
+
+export default class AudioPlayer extends PureComponent {
+  constructor(props) {
+    super(props);
+
+    const {isPlaying, src} = props;
+
+    this._audio = new Audio(src);
+
+    this.state = {
+      progress: this._audio.currentTime,
+      isLoading: true,
+      isPlaying
+    };
+
+    this._audio.oncanplaythrough = () => this.setState({
+      isLoading: false
+    });
+
+    this._audio.onpause = () => this.setState({
+      isPlaying: false
+    });
+
+    this._audio.ontimeupdate = () => this.setState({
+      progress: this._audio.currentTime
+    });
+
+    this._onPlayButtonClick = this._onPlayButtonClick.bind(this);
+  }
+
+  render() {
+    const {isLoading, isPlaying} = this.state;
+
+    return (
+      <Fragment>
+        <button
+          className={`track__button track__button--${isPlaying ? `pause` : `play`}`}
+          type="button"
+          disabled={isLoading}
+          onClick={this._onPlayButtonClick}
+        />
+        <div className="track__status">
+          <audio />
+        </div>
+      </Fragment>
+    );
+  }
+
+  componentDidUpdate() {
+    if (this.props.isPlaying) {
+      this._audio.play();
+    } else {
+      this._audio.pause();
+    }
+  }
+
+  _onPlayButtonClick() {
+    this.props.onPlayButtonClick();
+    this.setState({isPlaying: !this.state.isPlaying});
+  }
+}
+
+AudioPlayer.propTypes = {
+  isPlaying: PropTypes.bool.isRequired,
+  onPlayButtonClick: PropTypes.func.isRequired,
+  src: PropTypes.string.isRequired,
+};
+
--- src/components/genre-question-screen/genre-question-screen.jsx
+++ src/components/genre-question-screen/genre-question-screen.jsx
@@ -1,26 +1,39 @@
-import React from 'react';
+import React, {PureComponent} from 'react';
 import PropTypes from 'prop-types';
+import AudioPlayer from './../audio-player/audio-player.jsx';
 
-const GenreQuestionScreen = ({question, onAnswer}) => {
-  const {
-    answers,
-    genre,
-  } = question;
+export default class GenreQuestionScreen extends PureComponent {
+  constructor(props) {
+    super(props);
 
-  return (
-    <section className="game__screen">
+    this.state = {
+      activePlayer: -1,
+    };
+  }
+
+  render() {
+    const {question, onAnswer} = this.props;
+    const {
+      answers,
+      genre,
+    } = question;
+
+    return <section className="game__screen">
       <h2 className="game__title">Выберите {genre} треки</h2>
       <form className="game__tracks" onSubmit={(evt) => {
         evt.preventDefault();
         onAnswer();
       }}>
         {answers.map((it, i) => <div className="track" key={`answer-${i}`}>
-          <button className="track__button track__button-play" type="button" />
-          <div className="track__status">
-            <audio />
-          </div>
+          <AudioPlayer
+            src={it.src}
+            isPlaying={i === this.state.activePlayer}
+            onPlayButtonClick={() => this.setState({
+              activePlayer: this.state.activePlayer === i ? -1 : i
+            })}
+          />
           <div className="game__answer">
-            <input className="game__input visually-hidden" type="checkbox" name="answer" value={`answer-${i}`} id={`answer-${i}`} />
+            <input className="game__input visually-hidden" type="checkbox" name="answer" value={`answer-${i}`} id={`answer-${i}`}/>
             <label className="game__check" htmlFor={`answer-${i}`}>
               Отметить
             </label>
@@ -29,9 +42,9 @@ const GenreQuestionScreen = ({question, onAnswer}) => {
 
         <button className="game__submit button" type="submit">Ответить</button>
       </form>
-    </section>
-  );
-};
+    </section>;
+  }
+}
 
 GenreQuestionScreen.propTypes = {
   onAnswer: PropTypes.func.isRequired,
@@ -44,5 +57,3 @@ GenreQuestionScreen.propTypes = {
     type: PropTypes.oneOf([`genre`, `artist`]).isRequired,
   }).isRequired,
 };
-
-export default GenreQuestionScreen;
